e252af7bb9362d21d1fd35f21c2b5aea6c67573b
ScarletBird <fgfogolin@hotmail.com>
2019-08-28 18:40:22 -0300

    Testing react classes 2

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 
-class RegularClass {}
-
-class ComponentClass extends Component{}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
-
 class App extends Component {
+    constructor(){
+        super();
+
+        this.state = { displayBio: false };
+
+        //onsole.log('Component this', this);
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        //console.log('readMore this', this);
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
     render(){
+
         return(
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Gustavo. I'm a computer scientist.</p>
                 <p>I'm always looking forward to working on challenging projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Sao Paulo, adjusting to new habits.</p>
+                            <p>My favourite language is Java, learning JavaScript and React right now.</p>
+                            <p>My hobbies are: drawing, writing, coding, and gaming.</p>
+                            <button onClick={this.toggleDisplayBio}>Show less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
             </div>
         )
     }
